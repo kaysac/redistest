881b18838e4761d303f6a9584cce5d7585cbe90b
jiangzk <565942166@qq.com>
2022-12-14 16:26:35 +0800

    add redis

--- src/main/java/com/hmdp/utils/LoginIntercepter.java
+++ src/main/java/com/hmdp/utils/LoginIntercepter.java
@@ -1,29 +1,30 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static net.sf.jsqlparser.util.validation.metadata.NamedObject.user;
 
 public class LoginIntercepter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HttpSession httpSession = request.getSession();
-        UserDTO user = (UserDTO) httpSession.getAttribute("user");
-        if (user == null) {
-            throw new RuntimeException("未登录");
+//        HttpSession httpSession = request.getSession();
+//        UserDTO user = (UserDTO) httpSession.getAttribute("user");
+        if (UserHolder.getUser() == null) {
+            return false;
         }
-        UserHolder.saveUser(user);
         return true;
     }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
-    }
 }
